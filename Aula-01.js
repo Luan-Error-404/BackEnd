936c1599a9bfeeaa1fa26c2c1058b873bcfc62f4
Luan-Error-404 <jesus.l@estudante.ifro.edu.br>
2025-04-03 16:26:30 -0400

    Testes de tipos de dados

---
+++ Aula-01.js
@@ -0,0 +1,7 @@
+let valor = 13.141516;
+let texto = "Meu primeiro projeto em JavaScript";
+let boleano = true;
+
+console.log(valor.toFixed(2));
+console.log(texto.toUpperCase());
+console.log(boleano.toString());
