e0ae363f02eb6d995823b29d0a6d710ccb604b89
Luan Carlos <jesus.l@estudante.ifro.edu.br>
2025-04-04 10:32:29 -0400

    Create atividade-06.js

---
+++ Atividades/Ati-01/atividade-06.js
@@ -0,0 +1,9 @@
+/*
+6 - Declare duas idades e utilize operadores de comparação para verificar se uma
+pessoa é mais velha que a outra.
+*/
+
+let José = 16;
+let Adriano = 75;
+let mensagem = José > Adriano? "José é mais velho" : "Adriano é mais velho";
+console.log(mensagem);
