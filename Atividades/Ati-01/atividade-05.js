0edd8ba963e496f3bc4ef80c90a44d9c8d35ccd4
Luan Carlos <jesus.l@estudante.ifro.edu.br>
2025-04-04 10:28:01 -0400

    Create atividade-05.js

---
+++ Atividades/Ati-01/atividade-05.js
@@ -0,0 +1,12 @@
+/*
+5 - Crie um programa que declare duas variáveis e exiba o resultado da soma,
+subtração, multiplicação e divisão desses números
+*/
+
+let num1 = 25;
+let num2 = 12;
+
+console.log("Soma: " + (num1 + num2));
+console.log("Subtração: " + (num1 - num2));
+console.log("Multiplicação: " + (num1 * num2));
+console.log("Divisão: " + (num1 / num2));
