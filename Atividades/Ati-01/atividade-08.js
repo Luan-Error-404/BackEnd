1238bb9bf26c26f7397a48ff324276b195e40310
Luan Carlos <jesus.l@estudante.ifro.edu.br>
2025-04-04 10:38:47 -0400

    Create atividade-08.js

---
+++ Atividades/Ati-01/atividade-08.js
@@ -0,0 +1,8 @@
+/*
+8 - Crie uma variável com um número e use o operador ternário para verificar se é
+par ou ímpar
+*/
+
+let num = 18;
+let mensagem = 18%2 == 0? "Número Par" : "Número Ímpar";
+console.log(mensagem);
