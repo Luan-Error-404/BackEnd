0cce56620c8a0300e3b54b81b7c9ad6b2f8d0238
Luan Carlos <jesus.l@estudante.ifro.edu.br>
2025-04-04 10:35:43 -0400

    Create atividade-07.js

---
+++ Atividades/Ati-01/atividade-07.js
@@ -0,0 +1,9 @@
+/*
+7 - Crie uma variável idade com um número e use o operador ternário para verificar
+se o valor armazenado na variável é maior ou igual a 18. Imprima “Maior de
+idade” ou “Menor de idade”.
+*/
+
+let idade = 19;
+mensagem = idade >= 18? "Maior de idade" : "Menor de idade";
+console.log(mensagem);
